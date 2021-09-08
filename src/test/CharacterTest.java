41caa133ebe67bf6988f16eb5877a675bbac5993
munlikaCh <munlika_ch@cmu.ac.th>
2021-09-08 17:49:17 +0700

    finish

--- src/test/CharacterTest.java
+++ src/test/CharacterTest.java
@@ -1,20 +1,58 @@
 package test;
 
+import controller.DrawingLoop;
+import controller.GameLoop;
+//import javafx.application.Platform;
+import view.Platform;
 import model.Character;
 import javafx.scene.input.KeyCode;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import de.saxsys.mvvmfx.testingutils.jfxrunner.JfxRunner;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 @RunWith(JfxRunner.class)
 public class CharacterTest {
     private Character floatingCharacter;
+
+    private ArrayList<Character> characterListUnderTest;
+    private Platform platformUnderTest;
+    private GameLoop gameLoopUnderTest;
+    private DrawingLoop drawingLoopUnderTest;
+    private Method updateMethod;
+    //add
+    private Method redrawMethod;
+
     @Before
     public void setup(){
         floatingCharacter = new Character(30, 30, 0, 0, KeyCode.A, KeyCode.D, KeyCode.W);
+
+        characterListUnderTest = new ArrayList<Character>();
+        characterListUnderTest.add(floatingCharacter);
+        platformUnderTest = new Platform();
+        gameLoopUnderTest = new GameLoop(platformUnderTest);
+        drawingLoopUnderTest = new DrawingLoop(platformUnderTest);
+        try {
+            updateMethod = GameLoop.class.getDeclaredMethod("update", ArrayList.class);
+            redrawMethod = DrawingLoop.class.getDeclaredMethod("paint", ArrayList.class);
+            updateMethod.setAccessible(true);
+            redrawMethod.setAccessible(true);
+        }catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            updateMethod = null;
+            redrawMethod = null;
+        }
     }
+
     @Test
     public void characterInitialValuesShouldMatchConstructorArguments(){
         assertEquals("Initial x", 30, floatingCharacter.getX(), 0);
@@ -24,5 +62,21 @@ public class CharacterTest {
         assertEquals("Left key", KeyCode.A, floatingCharacter.getLeftKey());
         assertEquals("Right key", KeyCode.D, floatingCharacter.getRightKey());
         assertEquals("Up key", KeyCode.W, floatingCharacter.getUpKey());
+    }
+
+    @Test
+    public void characterShouldMoveToTheLeftAfterTheLeftKeyIsPressed() throws IllegalAccessException, InvocationTargetException, InvocationTargetException,
+            InvocationTargetException, NoSuchFieldException {
+        Character characterUnderTest = characterListUnderTest.get(0);
+        int startX = characterUnderTest.getX();
+        platformUnderTest.getKeys().add(KeyCode.A);
+        updateMethod.invoke(gameLoopUnderTest,characterListUnderTest);
+        redrawMethod.invoke(drawingLoopUnderTest,characterListUnderTest);
+        Field isMoveLeft = characterUnderTest.getClass().getDeclaredField("isMoveLeft");
+        isMoveLeft.setAccessible(true);
+        assertTrue("Controller: Left key pressing is acknowledged",platformUnderTest.getKeys().isPressed(KeyCode.A));
+        assertTrue("Model: Character moving left state is set", isMoveLeft.getBoolean(
+                characterUnderTest));
+        assertTrue("View: Character is moving left", characterUnderTest.getX() < startX);
     }
 }
